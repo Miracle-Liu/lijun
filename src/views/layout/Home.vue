4208d8d52a5d942efb29e92b49b0fe699a9db6db
miracle <1107650503@qq.com>
2019-06-20 18:38:47 +0800

    修改页面布局

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="lijun-home">
+    <Banner />
+    <KnowUs id="knowUs" />
+    <QualityGoods id="qualityGoods" />
+    <AboutUs />
+    <Questions />
+    <Download />
+  </div>
+</template>
+<script>
+import KnowUs from '@/components/KnowUs.vue'
+import QualityGoods from '@/components/QualityGoods.vue'
+import Banner from '@/components/Banner.vue'
+import AboutUs from '@/components/AboutUs.vue'
+import Questions from '@/components/Questions.vue'
+import Download from '@/components/Download.vue'
+
+export default {
+  name: "layout",
+  components:{
+    KnowUs,
+    QualityGoods,
+    Banner,
+    AboutUs,
+    Questions,
+    Download,
+  },
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+  methods: {
+  }
+};
+</script>
+<style lang="scss">
+.lijun-home {
+}
+</style>
+
+
--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -2,35 +2,18 @@
   <div class="lijun-layout">
 
     <Head id="homePage" />
-    <Banner />
-    <KnowUs id="knowUs" />
-    <QualityGoods id="qualityGoods" />
-    <AboutUs />
-    <Questions />
-    <Download />
+    <router-view class="lijun-content" />
     <Footer id="connectUs" />
   </div>
 </template>
 <script>
 import Head from '@/components/Head.vue'
-import KnowUs from '@/components/KnowUs.vue'
-import QualityGoods from '@/components/QualityGoods.vue'
-import Banner from '@/components/Banner.vue'
-import AboutUs from '@/components/AboutUs.vue'
-import Questions from '@/components/Questions.vue'
-import Download from '@/components/Download.vue'
 import Footer from '@/components/Footer.vue'
 
 export default {
   name: "layout",
   components:{
     Head,
-    KnowUs,
-    QualityGoods,
-    Banner,
-    AboutUs,
-    Questions,
-    Download,
     Footer
   },
   data() {
@@ -45,6 +28,9 @@ export default {
 </script>
 <style lang="scss">
 .lijun-layout {
+  .lijun-content {
+    min-height: calc(100% - 400px);
+  }
 }
 </style>
 
